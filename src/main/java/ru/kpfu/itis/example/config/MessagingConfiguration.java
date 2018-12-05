9937c44e77a941ef3d480e955ce9748157676c36
Maxim <maksoh88@gmail.com>
2018-12-05 11:50:56 +0300

    Added direct exchange example

--- src/main/java/ru/kpfu/itis/example/config/MessagingConfiguration.java
+++ src/main/java/ru/kpfu/itis/example/config/MessagingConfiguration.java
@@ -1,6 +1,6 @@
 package ru.kpfu.itis.example.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +10,16 @@ public class MessagingConfiguration {
     @Bean
     public Queue myQueue1() {
         return new Queue("queue-example-2");
+    }
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("direct-exchange-example-1");
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(myQueue1()).to(new DirectExchange("exchange-example-1")).with("example");
+//        return BindingBuilder.bind(myQueue1()).to(new DirectExchange("exchange-example-1")).withQueueName();
     }
 }
